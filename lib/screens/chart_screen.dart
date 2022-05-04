a993a9b67abc102cdada41ece6152d452bc73a0a
Michal Predotka <mivoligo@gmail.com>
2022-05-04 15:15:24 +0100

    Add TotalSpending widget

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/total_spending.dart';
+
 class ChartScreen extends StatelessWidget {
   const ChartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Chart Screen'));
+    return Column(
+      children: [
+        // SpendingTypes(),
+        // SpendingSummary(),
+        // SpendingChart(),
+        TotalSpending(),
+      ],
+    );
   }
 }
